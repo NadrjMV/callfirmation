a58e602d72f3315c3b9344f5c91cc81555a9eb86
NadrjMV <jordanmvpower@gmail.com>
2025-05-28 08:54:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from pytz import timezone
-
+ 
 load_dotenv()
 app = Flask(__name__)
 
